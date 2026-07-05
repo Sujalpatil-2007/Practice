73caa75e4a96de1d440ed78cc8bf89407e327b28
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-05 22:15:13 +0530

    added section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <main className='h-screen w-screen bg-gray-500 '>
-
+        <section>
+          
+        </section>
     </main>
   )
+
 }
 
 export default App
