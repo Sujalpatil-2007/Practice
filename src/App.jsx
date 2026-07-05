5b0148c0a9d7e9d95ae5b6f0941a9cb1e56172f4
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-05 22:21:58 +0530

    proper

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <main className='h-screen w-screen bg-gray-500 '>
-      <div className='bg-black'>
-
-      </div>
-        <section>
-          <h1 className='text-5xl font-bold'>Hello word</h1>
-          <p className='text-2xl font-semibold'>This is testing</p>
-        </section>
+    <main className="h-screen w-screen bg-gray-500 ">
+      <div className="bg-black"></div>
+      <section>
+        <h1 className="text-5xl font-bold">Hello word</h1>
+        <p className="text-2xl font-semibold">This is testing</p>
+      </section>
     </main>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
